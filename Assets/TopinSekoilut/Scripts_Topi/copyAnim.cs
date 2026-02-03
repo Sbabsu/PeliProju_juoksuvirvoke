8b76586755391c3d4136f26381ca205d746f3957
Topi <topiek00@gmail.com>
2026-02-03 23:41:06 +0200

    Hahmon kamera kontrollit ja rotaatiot sen kanssa toimii, ttu mikä työ maa

--- Assets/TopinSekoilut/Scripts_Topi/copyAnim.cs
+++ Assets/TopinSekoilut/Scripts_Topi/copyAnim.cs
@@ -6,8 +6,6 @@ public class copyAnim : MonoBehaviour
     public bool mirror;
 
     [SerializeField] Transform animRoot;   // root of the animated copy rig (NOT ragdoll hips)
-    [SerializeField] float turnSpeed = 12f;
-
 
     private ConfigurableJoint configurableJoint;
     private Quaternion startRotation;
