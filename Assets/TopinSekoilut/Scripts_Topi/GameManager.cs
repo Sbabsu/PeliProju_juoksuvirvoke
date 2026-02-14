1264fc47d93e1517ab22ecce747dd49a33dfef34
Topi <topiek00@gmail.com>
2026-02-15 00:37:44 +0200

    tölkkien materiaaleja touch upattu v

--- Assets/TopinSekoilut/Scripts_Topi/GameManager.cs
+++ Assets/TopinSekoilut/Scripts_Topi/GameManager.cs
@@ -21,7 +21,7 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        Application.targetFrameRate = 120;
+        Application.targetFrameRate = 144;
     }
 
     // Exitille pääsee aina
