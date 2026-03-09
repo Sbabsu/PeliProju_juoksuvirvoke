02bdf51faf0eb1c59027953c39357dee5f6946b3
Topi <topiek00@gmail.com>
2026-03-09 22:01:10 +0200

    kartta4 valmis

--- Assets/skriptit/UI&ITEMS/ItemDefinition.cs
+++ Assets/skriptit/UI&ITEMS/ItemDefinition.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Loot/Item Definition")]
 public class ItemDefinition : ScriptableObject
 {
-    public string id;          // esim "beer_can"
-    public string displayName; // "Olut tölkki"
-    public Sprite icon;        // UI-ikoni (voi olla tyhjä aluksi)
+    public string id;
+    public string displayName;
+    public Sprite icon;
 
     [Header("World Drop")]
-    public GameObject worldPrefab; // prefab spawned when dropped/bursted
+    public GameObject worldPrefab;
+
 }
